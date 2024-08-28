42b890d25ecff6982304a0d28556c937a6a99069
GhostmanBY <nahuelromero2709@gmail.com>
2024-08-28 01:18:09 -0300

    definicon

--- Backend/Panel_Admin_Back.py
+++ Backend/Panel_Admin_Back.py
@@ -1,43 +1,48 @@
 import os
 import time
 import random
-from datetime import datetime
 import sqlite3
 
 ruta_db = os.path.join("DB", "Panel_admin.db")
 
+#Funcion que genera un codigo con 4 letras y 3 numeros, para la identificacion de los mozos
 def Generar_Codigo():    
     lista_Letras = ["A", "B", "C", "D", "E", "F", "G",
                     "H", "I", "J", "K", "L", "M", "N", 
                     "O","P", "Q", "S", "T", "R", "U", 
                     "V", "W", "X", "Y", "Z"]
+    #Se inicializa codigo para poder iterar los espacios del str
     codigo = list("AOOE505")
-    for i in range(len(lista_Letras)):
+    #Un ciclo for para iterar la variable de codigo
+    for i in range(0, 6):
+            #Se utiliza el motodo radiant de la libreria ramdom para elejir una letra de forma alatoria
             valor_R_letra = random.randint(0, len(lista_Letras)-1)
+            #If para que pare de sumar letras y pase a añadir numeros del 0 al 9
             if i <= 3:
                 codigo[i] = lista_Letras[valor_R_letra]
             elif i <= 6:
                 valor_R_numero = random.randint(0, 9)
 
+                #Se le asigna el numero pero como un str para que haga conflictos con la iteracion de la variable
                 codigo[i] = str(valor_R_numero)
-            
-            if i == 6:
-                break
+    
+    #Una vez que el codigo ya tiene sus caracteres corespondientes, se vuelve a unir la lista en una sola cadena con el .join
     codigo = ''.join(codigo)
 
     return codigo
 
+#Creacion de las tables 
 def crear_tablas():
+    conn = sqlite3.connect(ruta_db) #Se conecta a la base de datos creada
+    cursor = conn.cursor() #Crea el cursor para ejecutar comandos en la base de datos
 
-    conn = sqlite3.connect(ruta_db)
-    cursor = conn.cursor()
-
+    #Ejecuta una instruccion
+    #Se crean las tabla si no existen (por eso la aclaracion de IF NOT), una vez la tabla creada crea la columna con su tipo de dato
     cursor.execute(
         """CREATE TABLE IF NOT EXISTS Productos(
-        Nombre text,
-        Precio integer,)"""
-    )
-
+        Nombre TEXT,
+        Precio INTEGER,)"""
+    ) 
     cursor.execute(
         """CREATE TABLE IF NOT EXISTS Usuarios(
         Mozo TEXT,
@@ -45,74 +50,81 @@ def crear_tablas():
         Plaza INTEGER)"""
     )
 
-    conn.commit()
-    conn.close()
+    conn.commit() #Guarda los cambios hechos a la base de datos
+    conn.close() #Cierra la coneccion con la base de datos 
 
 def Cargar_Producto(name, precio):
+    #Se conecta a la base de datos y crea el cursor
     conn = sqlite3.connect(ruta_db)
     cursor = conn.cursor()
     
-    instruccion = "INSERT INTO Productos (nombre, precio) VALUES (?, ?)"
-    cursor.execute(instruccion, (name, precio))
+    #Se crea una variable para darle instrucciones, estas se dan con parametros especificos propios de SQL
+    instruccion = "INSERT INTO Productos (nombre, precio) VALUES (?, ?)" #Ingresa a la base de datos los valores que resive por eso es INSERT
+    cursor.execute(instruccion, (name, precio)) #Ejecuta la accion 
 
-    conn.commit()
-    conn.close()
+    conn.commit() #Guarda los cambios hechos a la base de datos
+    conn.close() #Cierra la coneccion con la base de datos 
 
 def Modificar_Productos(name, categoria, nuevo_valor):
+    #Se conecta a la base de datos y crea el cursor
     conn = sqlite3.connect(ruta_db)
-    cursor = conn.cursor()
+    cursor = conn.cursor() 
 
-    instruccion = f"UPDATE Productos SET {categoria} = {nuevo_valor} WHERE Nombre like '{name}'"
-    cursor.execute(instruccion)
+    instruccion = f"UPDATE Productos SET {categoria} = {nuevo_valor} WHERE Nombre like '{name}'" #Actualiza los valores que se le indiquen en la base de datos por eso UPDATE
+    cursor.execute(instruccion) #Ejecuta la accion 
 
-    conn.commit()
-    conn.close()
+    conn.commit() #Guarda los cambios hechos a la base de datos
+    conn.close() #Cierra la coneccion con la base de datos 
 
 def Mostrar_Productos():
+    #Se conecta a la base de datos y crea el cursor
     conn = sqlite3.connect(ruta_db)
-    cursor = conn.cursor()
+    cursor = conn.cursor() 
 
-    instruccion = f"SELECT * FROM Productos"
-    cursor.execute(instruccion)
+    instruccion = f"SELECT * FROM Productos" #Captura todos los datos de la base de datos por eso SELECT
+    cursor.execute(instruccion) #Ejecuta la accion 
 
-    datos = cursor.fetchall()
+    datos = cursor.fetchall() #La variable datos pasa a tener todos los valores que tiene cursos, metiendo en una lista con sub indices
 
-    conn.commit()
-    conn.close()
+    conn.commit() #Guarda los cambios hechos a la base de datos
+    conn.close() #Cierra la coneccion con la base de datos 
 
     return datos
 
 def Eliminar_Producto(name):
+    #Se conecta a la base de datos y crea el cursor
     conn = sqlite3.connect(ruta_db)
-    cursor = conn.cursor()
+    cursor = conn.cursor() 
     
-    instruccion = f"DELETE FROM Productos WHERE Nombre like '{name}'"
-    cursor.execute(instruccion)
+    instruccion = f"DELETE FROM Productos WHERE Nombre like '{name}'" #Elimina la fila en la que este el nombre que se le ingresa por eso DELETE
+    cursor.execute(instruccion) #Ejecuta la accion 
 
-    conn.commit()
-    conn.close()
+    conn.commit() #Guarda los cambios hechos a la base de datos
+    conn.close() #Cierra la coneccion con la base de datos 
 
 def Registro_Empleado(name, codigo, Plaza):
+    #Se conecta a la base de datos y crea el cursor
     conn = sqlite3.connect(ruta_db)
-    cursor = conn.cursor()
+    cursor = conn.cursor() 
 
-    instruccion = f"INSERT INTO Usuarios (mozo, codigo, plaza) VALUES (?, ?, ?)"
-    cursor.execute(instruccion, (name, codigo, Plaza))
+    instruccion = f"INSERT INTO Usuarios (mozo, codigo, plaza) VALUES (?, ?, ?)" #Ingresa a la base de datos los valores que resive por eso es INSERT
+    cursor.execute(instruccion, (name, codigo, Plaza)) #Ejecuta la accion 
 
-    conn.commit()
-    conn.close()
+    conn.commit() #Guarda los cambios hechos a la base de datos
+    conn.close() #Cierra la coneccion con la base de datos 
 
 def Mostrar_Empleados():
+    #Se conecta a la base de datos y crea el cursor
     conn = sqlite3.connect(ruta_db)
-    cursor = conn.cursor()
+    cursor = conn.cursor() 
 
-    instruccion = f"SELECT * FROM Usuarios"
-    cursor.execute(instruccion)
+    instruccion = f"SELECT * FROM Usuarios" #Captura todos los datos de la base de datos por eso SELECT
+    cursor.execute(instruccion) #Ejecuta la accion 
 
     datos = cursor.fetchall()
 
-    conn.commit()
-    conn.close()
+    conn.commit() #Guarda los cambios hechos a la base de datos
+    conn.close() #Cierra la coneccion con la base de datos 
 
     return datos
 
@@ -121,42 +133,44 @@ def Modificar_Empleados(name, categoria, valor):
     cursor = conn.cursor()
     
     instruccion = f"SELECT * from Usuarios WHERE Mozo like '{name}'"
-    cursor.execute(instruccion)
+    cursor.execute(instruccion) #Ejecuta la accion 
     
-    datos = cursor.fetchall()
+    datos = cursor.fetchall() #La variable datos pasa a tener todos los valores que tiene cursos, metiendo en una lista con sub indices
     
-    if datos:
-        instruccion = f"UPDATE Usuarios SET {categoria} = ? WHERE Mozo like ?"
-        cursor.execute(instruccion, (valor, name))
-        conn.commit()
+    if datos: #Se ve si datos tiene o no valores
+        instruccion = f"UPDATE Usuarios SET {categoria} = ? WHERE Mozo like ?" #Se actualiza los datos
+        cursor.execute(instruccion, (valor, name)) #Ejecuta la accion 
+        conn.commit() #Guarda los cambios hechos a la base de datos
     else:
         return "No se encunetra el nombre del mozo ingresado"
         
-    conn.close()
+    conn.close() #Cierra la coneccion con la base de datos 
 
 def Eliminar_empleados(name):
+    #Se conecta a la base de datos y crea el cursor
     conn = sqlite3.connect(ruta_db)
-    cursor = conn.cursor()
+    cursor = conn.cursor() 
     
-    instruccion = f"DELETE FROM Productos WHERE Mozo like '{name}'"
-    cursor.execute(instruccion)
+    instruccion = f"DELETE FROM Productos WHERE Mozo like '{name}'" #Elimina la fila en la que este el nombre que se le ingresa por eso DELETE
+    cursor.execute(instruccion) #Ejecuta la accion 
     
-    conn.commit()
-    conn.close()
+    conn.commit() #Guarda los cambios hechos a la base de datos
+    conn.close() #Cierra la coneccion con la base de datos 
     
 def verificar(name, code):
+    #Se conecta a la base de datos y crea el cursor
     conn = sqlite3.connect(ruta_db)
-    cursor = conn.cursor()
+    cursor = conn.cursor() 
     
-    instruccion = f"SELECT * from Usuarios WHERE Mozo like '{name}'"
-    cursor.execute(instruccion)
+    instruccion = f"SELECT * from Usuarios WHERE Mozo like '{name}'" #Captura en especifico de la columna mozo el nombre que se le ingresa
+    cursor.execute(instruccion) #Ejecuta la accion 
     
-    datos = cursor.fetchall()
+    datos = cursor.fetchall() #La variable datos pasa a tener todos los valores que tiene cursos, metiendo en una lista con sub indices
     
-    conn.commit()
-    conn.close()
+    conn.commit() #Guarda los cambios hechos a la base de datos
+    conn.close() #Cierra la coneccion con la base de datos 
     
-    if datos != []:
+    if datos != []: #Comprobacion si tiene datos o no
         if code == datos[0][1]:
             return True
         else:
@@ -164,22 +178,25 @@ def verificar(name, code):
     else:
         return 2
 
+#Si se ejecuta este archivo desde el mismo se ejecutan todo lo que este por debajo de este if si no, no se hace
 if __name__ == "__main__":
-    crear_tablas()
+    crear_tablas() #Se crean las tablas
 
+    #Menu inicial
     po = 0
-    while po != 3:
+    while po != 3: #Ciclo para que se muestre hasta que el usuario quiera salir
         os.system("cls")
         po = int(input("""
 Ingrese la opcion que quiere realizar
 1- Carta
 2- Personal
 3- Salir
-RTA: """))
+RTA: """)) 
 
+        #Menu de productos
         if po == 1:
             op = 0
-            while op != 5:
+            while op != 5: #Ciclo para que se muestre hasta que el usuario quiera salir
                 op = int(input("""
 Ingrese la opcion que quiere realizar:
 1- Cargar producto
@@ -188,11 +205,12 @@ Ingrese la opcion que quiere realizar:
 4- Eliminar producto
 5- Volver
 RTA: """))
-
+                #Acciones del menu Carta
                 if op == 1:
                     nombre = input("Ingrese el nombre del producto: ")
                     precio = int(input("Ingrese el precio del producto: "))
                     Cargar_Producto(nombre, precio)
+                    
                     input("Presione enter...")
                     os.system("cls")
 
@@ -205,6 +223,7 @@ RTA: """))
                             elif j == 1:
                                 print(f"Precio: {datos[i][j]}")
                         print("-"*15)
+
                     input("Preisone Enter...")
                     os.system("cls")
 
@@ -213,17 +232,22 @@ RTA: """))
                     categoria = input("Ingrese lo que va a modificar si el preico o el stock: ")
                     valor_nuevo = int(input(f"Ingrese el {categoria} del producto: "))
                     Modificar_Productos(nombre, categoria.capitalize(), valor_nuevo)
+                    
                     input("Presione enter...")
                     os.system("cls")
 
                 elif op == 4:
                     nombre = input("Ingrese el nombre del producto a eliminar: ")
                     Eliminar_Producto(nombre)
+
                     input("Presione enter...")
                     os.system("cls")
+
         elif po == 2:
+
+            #Menu de empleados
             pop = 0
-            while pop != 6:
+            while pop != 6: #Ciclo para que se muestre hasta que el usuario quiera salir
                 pop = int(input("""
 Ingrese la opciona que quiere realizar:
 1- Registrar nuevo empleado
@@ -233,6 +257,7 @@ Ingrese la opciona que quiere realizar:
 5- Verificar
 6- Volver
 RTA: """))
+                #Acciones del menu de empleados
                 if pop == 1:
                     datos = Mostrar_Empleados()
                     
@@ -243,6 +268,7 @@ RTA: """))
                     Plaza = int(input("Ingrese la plaza a la que va a estar asiganado: "))
         
                     Registro_Empleado(name, codigo, Plaza)
+
                     input("Presione enter...")
                     os.system("cls")
                 elif pop == 2:
@@ -256,25 +282,32 @@ RTA: """))
                             elif j == 2:
                                 print(f"Plaza: {datos[i][j]}")
                         print("-"*15)
+
                     input("Preisone Enter...")
                     os.system("cls")
+
                 elif pop == 3:
                     name = input("Ingrese el nombre del mozo: ")
                     categoria = input("Que quiere cambiar el codigo o la plaza?: ")
+                    
                     if categoria.capitalize() == "Codigo":
                         valor_nuevo = Generar_Codigo()
                         print("Espere cambiando codigo...")
+
                         time.sleep(5)
+
                         input("Cambiado con exito!!(Preisone enter...)")
                     elif categoria.capitalize() == "Plaza":
                         valor_nuevo = int(input("Ingrese la nueva plaza: "))
                         input("Preisone Enter...")
+
                     Modificar_Empleados(name, categoria, valor_nuevo)
                     os.system("cls")
                     
                 elif pop == 4:
                     name = input("Ingrese el nombre del mozo a eliminar: ")
                     Eliminar_empleados(name)
+
                     input("Preisone Enter...")
                     os.system("cls")
                 
@@ -289,5 +322,6 @@ RTA: """))
                         print("Usted no esta en el sistema")
                     else:
                         print("Su codigo no es correcto")
+
                     input("Preisone Enter...")
                     os.system("cls")
--- Test.py
+++ Test.py
@@ -1,64 +1,9 @@
-from Backend.Panel_Admin_Back import Cargar_Producto, Mostrar_Productos, Modificar_Productos, Eliminar_Producto
+from customtkinter import *
 
-from customtkinter import* 
-
-def funcion():
-    txto1.pack_forget()
-    txto2.pack_forget()
-    txto3.pack_forget()
-    boton1.pack_forget()
-    boton2.pack_forget()
-    
-    texto1 = CTkTextbox(root, width=400, height=400, font=('Arial', 12))
-    texto1.pack()
-    
-    datos = Mostrar_Productos()
-
-    for i in range(0, len(datos)):
-                for j in range(0, 3):
-                    if j == 0:
-                        texto1.insert("1.0", f"Nombre: {datos[i][j]}\n\n")
-                    elif j == 1:
-                        texto1.insert("2.0", f"Precio: {datos[i][j]}\n")
-                    elif j == 2:
-                        texto1.insert("3.0", f"Stock: {datos[i][j]}\n")
-                texto1.insert("4.0", "-"*15)
-
-def funcion_2():
-    txto2.pack_forget()
-    txto3.pack_forget()
-
-    boton1.pack_forget()
-    boton2.pack_forget()
-    boton3.pack_forget()
-    boton4.pack_forget()
-    
-    boton5 = CTkButton(root, width=200, height=50, text="Eliminar", command=lambda: Eliminar_Producto(txto1.get()))
-    boton5.pack()
 root = CTk()
-root.title("Inicio")
 root.geometry("500x500")
+root.title("Panel Administrador")
 
-txto1 = CTkEntry(root, width=200, height=50)
-txto1.pack()
 
-txto2 = CTkEntry(root, width=200, height=50)
-txto2.pack()
-
-txto3 = CTkEntry(root, width=200, height=50)
-txto3.pack()
-
-boton1 = CTkButton(root, width=200, height=50, text="Cargar", command=lambda: Cargar_Producto(txto1.get(), txto2.get(), txto3.get()))
-boton1.pack(pady=20)
-
-boton2 = CTkButton(root, width=200, height=50, text="mostrar", command= funcion)
-boton2.pack(pady=20)
-
-boton3 = CTkButton(root, width=200, height=50, text="modificar", command=lambda: Modificar_Productos(txto1.get(), txto2.get(), txto3.get()))
-boton3.pack(pady=20)
-
-boton4 = CTkButton(root, width=200, height=50, text="Eliminar", command= funcion_2)
-boton4.pack(pady=20)
 
 root.mainloop()
-
