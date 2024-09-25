fb6410d47ebadac0b1b7cf263139c2ede37e5b5b
iarabulacio <bulacioiarajade@gmail.com>
2024-09-25 01:20:47 -0300

    login

--- Frontend/Login.py
+++ Frontend/Login.py
@@ -28,7 +28,7 @@ def verificar(code):
         if code == datos[0][1]:
             ventana_emergente("Bienvenido")
         else:
-           ventana_emergente("Su codigo no es correcto")
+            ventana_emergente("Su codigo no es correcto")
     else:
         ventana_emergente("Usted no esta en el sistema")
 
@@ -40,29 +40,36 @@ def accion(codigo, ventana):
     ventana.destroy() #destruye la ventana
     main(codigo) #llama a la funcion del archivo Ventana_Principal
 
-#crea la ventana del login
-root = CTk() #crea la ventana
-root.geometry("500x500") #le da la medidas de la misma
-root.title("Inicio") #le da un titulo a la ventana
+from tkinter import messagebox
+from customtkinter import *
+from PIL import Image
 
-set_appearance_mode("dark") #setea el tema de la aplicacion al tema oscuro
+#def verificación():
 
-#crea un frame(un recuadro) donde se le da una ubicacion espesifica con tamaño
-frame_decorativo = CTkFrame(master=root,width=350, height=350, fg_color="#0E498A") #le da los parametros
-frame_decorativo.place(relx=0.5, rely=0.5, anchor="center") #le da la ubicacion al frame
 
-#crea un recuadro de texto
-text_1 = CTkLabel(master=frame_decorativo, text="Ingrese su codigo", font=letra) #se le da los parametros
-text_1.place(relx=0.5, rely=0.3, anchor="center") #le da la ubicacion al texto
 
-#crea un campo para escribir 
-campo_codigo = CTkEntry(master=frame_decorativo, placeholder_text="AOE505") #le da los parametros
-campo_codigo.place(relx=0.5, rely=0.5, anchor="center") #le da la ubicacion al campo de texto
 
-"""crea un boton y a su vez pasa los parametros 
-y en especifico el parametro command= lambda permite que se le pasen valores a la funcion que usa el boton"""
-boton_acept = CTkButton(master=frame_decorativo, text="Ingresar", command= lambda: verificar(campo_codigo.get())) 
-boton_acept.place(relx=0.5, rely=0.6, anchor="center") #le da la ubicacion al boton
+root = CTk()
+root.title("Login")
+root.geometry("400x400")
+root.configure(fg_color="#FF6103")
 
-#mantien en un bucle la ventana para que se muestre
+
+frame1 = CTkFrame(master=root, width=400, height=100, fg_color="white")
+frame1.pack(pady=10)
+
+foto = Image.open("nombre.png")
+ft = CTkImage(foto, size=(200, 200))  
+label1 = CTkLabel(master=frame1, image=ft, text="")
+label1.pack(pady=10)
+
+
+entry_usuario = CTkEntry(master=frame1, placeholder_text="AOE505", fg_color="white", border_color="white", width=100, height=40)
+entry_usuario.pack(pady=20)
+
+boton1 = CTkButton(master=frame1,text="LOG IN", fg_color="#FF6103", command=verificar)
+boton1.pack(pady=20)
+
 root.mainloop()
+
+
