310f30271c91b631b08bdb163ed7d28e261cb029
keilabelen <keila.belen2317@gmail.com>
2024-10-07 16:33:53 -0300

    practica
    nada nuevo

--- Frontend/prueba.py
+++ Frontend/prueba.py
@@ -1,1 +1,11 @@
-print("hola mundo")
+import customtkinter as ctk
+
+class category:
+  def ventana (self):
+    self.ventana1=ctk.CTk()
+    self.ventana1.geometry("1000x800")
+    self.ventana1.title("CATEGORIAS")
+    
+    self.ventana1.mainloop
+
+resultado=category()
