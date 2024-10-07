72507f306b19dc43021423f6e1cc8759d7d8ec55
keilabelen <keila.belen2317@gmail.com>
2024-10-07 16:52:37 -0300

    creando las categorias

---
+++ Frontend/categorias.py
@@ -0,0 +1,8 @@
+import customtkinter as ctk
+
+#comenzamos creando la ventana y el frame con el color correspondoente
+class categorias:
+  def ventana(self):
+    self.ventana1= ctk.CTk()
+    self.ventana1.geometry("1000x800")
+    self.ventana1.title("CATEGORIAS")
