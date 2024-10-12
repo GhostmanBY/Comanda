562462e13d251432684f452359aed80b8c94e51c
keilabelen <keila.belen2317@gmail.com>
2024-10-12 20:59:15 -0300

    Update category.py

--- Frontend/category.py
+++ Frontend/category.py
@@ -1,11 +1,1 @@
-from customtkinter import *
 
-#Desarrollo un menu con las categorias de comida disponibles
-
-ventana1=CTk()
-ventana1.geometry("1000x800")
-ventana1.title("CATEGORIAS")
-ventana1.configure(fg_color="black")
-set_appearance_mode("dark")
-
-ventana1.mainloop()
