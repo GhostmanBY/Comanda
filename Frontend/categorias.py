b11c46ee7ef3d61d9e656e64ec41e450d41f50b0
keilabelen <keila.belen2317@gmail.com>
2024-10-12 20:58:19 -0300

    Update categorias.py

--- Frontend/categorias.py
+++ Frontend/categorias.py
@@ -1,8 +1,1 @@
-import customtkinter as ctk
 
-#comenzamos creando la ventana y el frame con el color correspondoente
-class categorias:
-  def ventana(self):
-    self.ventana1= ctk.CTk()
-    self.ventana1.geometry("1000x800")
-    self.ventana1.title("CATEGORIAS")
